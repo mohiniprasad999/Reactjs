8d40d9d5c6464b3d295a23f9a966ca1ea2f188ea
mohiniprasad <mohini.prasad@stackinfinite.com>
2023-08-02 10:59:58 +0530

    React Game

---
+++ src/components/Block.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+interface IBlockProps {
+    value?: string | null
+    onClick?: () => void
+}
+const Block: React.FC<IBlockProps> = (props) => {
+    return (
+        <div onClick={props.onClick} className='block'>{props.value}</div>
+    )
+}
+
+export default Block
